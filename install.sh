153d5e249dc96aca38c3a90a8ff07a5abafa8887
Atsune Mogi <atsune@Atsunes-MacBook-Air-2.local>
2026-05-05 19:02:00 +0900

    add cmux

--- install.sh
+++ install.sh
@@ -36,3 +36,4 @@ curl -fLo "${HOME}/.vim/autoload/plug.vim" --create-dirs \
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Install completed!"
+
